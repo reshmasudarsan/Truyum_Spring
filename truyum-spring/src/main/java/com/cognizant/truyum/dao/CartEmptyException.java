11050ebaccb203a6865bca4a77f72426bb70b861
Reshma <lechusudarshan@gmail.com>
2020-11-12 17:51:49 +0530

    code uploaded

---
+++ truyum-spring/src/main/java/com/cognizant/truyum/dao/CartEmptyException.java
@@ -0,0 +1,10 @@
+package com.cognizant.truyum.dao;
+
+public class CartEmptyException extends Exception{
+
+	private static final long serialVersionUID = 6539196163290646747L;
+	public CartEmptyException() {
+		super();
+	}
+	
+}
---
+++ truyum-spring/src/main/java/com/cognizant/truyum/dao/MenuItemDaoCollectionImpl.java
@@ -0,0 +1,75 @@
+package com.cognizant.truyum.dao;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import com.cognizant.truyum.model.MenuItem;
+import com.cognizant.truyum.util.DateUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@ImportResource("classpath:spring-config.xml")
+public class MenuItemDaoCollectionImpl implements MenuItemDao{
+
+@Autowired
+private ArrayList<MenuItem> menuItemList;
+
+
+public ArrayList<MenuItem> getMenuItemList() {
+return menuItemList;
+}
+
+public void setMenuItemList(ArrayList<MenuItem> menuItemList) {
+this.menuItemList = menuItemList;
+}
+
+public ArrayList<MenuItem> getMenuItemListAdmin(){
+
+return menuItemList;
+}
+
+public ArrayList<MenuItem> getMenuItemListCustomer() {
+ArrayList<MenuItem> menu = new ArrayList<MenuItem>();
+DateUtil d = new DateUtil();
+for(MenuItem m:menuItemList) {
+try {
+	if(m.isActive()==true &&(m.getDateOfLaunch().before(d.convertToDate("19/08/2019")))) {
+	menu.add(m);
+	}
+} catch (ParseException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+}
+return menu;
+}
+@Override
+public void modifyMenuItem(MenuItem menuItem) {
+
+for(MenuItem m:menuItemList) {
+if(m.getId()==(menuItem.getId())) {
+m.setId(menuItem.getId());
+m.setName(menuItem.getName());
+m.setCategory(menuItem.getCategory());
+m.setPrice(menuItem.getPrice());
+m.setActive(menuItem.isActive());
+m.setDateOfLaunch(menuItem.getDateOfLaunch());
+}
+}
+}
+@Override
+public MenuItem getMenuItem(long menuItemId) {
+MenuItem menu=null;
+for(MenuItem m:menuItemList) {
+if(m.getId()==menuItemId) {
+menu=m;
+break;
+}
+}
+return menu;
+}
+}
